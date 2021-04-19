512f93ff821c95274740ed94473ac88b807ef665
alexhahahaDrag <734663446@qq.com>
2021-04-19 22:38:46 +0800

    add vue-router(attention vue-router need 4 or more)

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@v/home/index.vue')
+    }
+]
+
+export default createRouter(
+    {
+        history: createWebHistory(),
+        routes
+    }
+)
---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>首页</h1>
+  <div id="app">hello vue</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
