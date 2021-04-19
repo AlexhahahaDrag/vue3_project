4a92dcd7db98565ecf9fb4105ed0148413ea4aa7
alexhahahaDrag <734663446@qq.com>
2021-04-20 07:11:36 +0800

    add element-plus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ElButton, ElRow } from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 createApp(App)
+    .use(ElButton)
+    .use(ElRow)
     .use(router)
     .mount('#app')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,14 @@
 <template>
 <h1>首页</h1>
   <div id="app">hello vue</div>
+  <el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
 </template>
 
 <script>
