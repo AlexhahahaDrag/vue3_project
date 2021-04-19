407d20243ef8ebc8755261ce15d6aa5740bc39b0
alexhahahaDrag <734663446@qq.com>
2021-04-19 21:41:07 +0800

    修改alias为resolve.alias

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,14 @@ function resolve(dir) {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    '@': resolve('./src'),
-    '@v': resolve('./src/views'),
-    '@c': resolve('./components'),
-    '@u': resolve('./src/utils'),
-    '@a': resolve('./src/api')
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+      '@v': resolve('./src/views'),
+      '@c': resolve('./components'),
+      '@u': resolve('./src/utils'),
+      '@a': resolve('./src/api')
+    }
   },
   server: {
     port: 3001,
